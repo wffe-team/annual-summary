7d9d4a0a6f2edd1940bcd778f940e7b29680604a
刘悦 <liuyue@wanfangdata.com.cn>
2017-02-15 15:48:47 +0800

    html css

---
+++ AnnualSummary/AnnualSummary/public/src/js/initPersonalCharts.js
@@ -0,0 +1,344 @@
+﻿(function () {
+    var myChart = echarts.init(document.getElementById('biChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '毕山山'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    };    myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('yangChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '杨明'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('liChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '李利'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('luChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '鹿丹'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('liuChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '刘悦'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('zhangChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '张凯迪'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('cuiChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '崔微娜'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
+(function () {
+    var myChart = echarts.init(document.getElementById('anChart'));
+    var option = {
+        color: ['#707d8d'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        title: {
+            text: '安晓凯'
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['高教公图', '企业政府', '医药', 'WFUI', 'Lab'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '30px',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    }; myChart.setOption(option);
+})();
--- AnnualSummary/AnnualSummary/public/src/js/initecharts.js
+++ AnnualSummary/AnnualSummary/public/src/js/initecharts.js
@@ -8,7 +8,7 @@
         legend: {
             orient: 'vertical',
             x: 'left',
-            data: ['高教公图', '企业政府', '医药']
+            data: ['高教公图', '企业政府', '医药','WFUI','Lab']
         },
         series: [
             {
@@ -37,7 +37,9 @@
                 data: [
                     { value: 335, name: '高教公图' },
                     { value: 310, name: '企业政府' },
-                    { value: 234, name: '医药' }
+                    { value: 234, name: '医药' },
+                    { value: 234, name: 'WFUI' },
+                    { value: 234, name: 'Lab' }
                 ]
             }
         ]
