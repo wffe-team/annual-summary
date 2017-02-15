a495725d092f500d55de8325c411577d40d925f2
zhangkaidi <zhangkaidi@ZHANGKAIDI>
2017-02-15 18:25:55 +0800

    update members

--- AnnualSummary/AnnualSummary/routes/index.js
+++ AnnualSummary/AnnualSummary/routes/index.js
@@ -1,9 +1,24 @@
 ﻿var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    fs.readFile('trellodata.json', function (err, data) {
+        if (err) {
+            return console.error(err);
+        } else {
+            //  var data = JSON.parse(data.toString('utf8'));
+            var string = JSON.parse(data);
+            var fullNameArr = [];
+            for (var i = 0; i < string.members.length; i++) {
+                var fullName = string.members[i].username;
+                fullNameArr.push(fullName);
+            }
+            res.render('index', { value: fullNameArr });
+        }
+    });
+
 });
 
 module.exports = router;
