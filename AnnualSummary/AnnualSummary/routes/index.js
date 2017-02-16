599f04d361a00ccd7b5fd59d907b4347780cc28a
zhangkaidi <zhangkaidi@ZHANGKAIDI>
2017-02-16 11:00:08 +0800

    update

--- AnnualSummary/AnnualSummary/routes/index.js
+++ AnnualSummary/AnnualSummary/routes/index.js
@@ -4,17 +4,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    fs.readFile('trellodata.json', function (err, data) {
+    fs.readFile('trellodata.json', 'utf8', function (err, data) {
         if (err) {
             return console.error(err);
         } else {
-            //  var data = JSON.parse(data.toString('utf8'));
-            var string = JSON.parse(data);
+            var obj = JSON.parse(data);
+            //前端成员
             var fullNameArr = [];
-            for (var i = 0; i < string.members.length; i++) {
-                var fullName = string.members[i].username;
+            for (var i = 0; i < obj.members.length; i++) {
+                var fullName = obj.members[i].fullName;
                 fullNameArr.push(fullName);
             }
+            //2016总的项目数
+            //var items = [];
+            //for (var i = 0; i < obj.actions.length; i++) {
+            //    if (obj.actions[i].data.list != undefined) {
+            //        var year = obj.actions[i].data.list.name;
+            //        if (year == 2016) {
+            //            var yearName = obj.actions[i].data.card.name;
+            //            console.log(yearName);
+            //        }
+            //    }
+            //}
             res.render('index', { value: fullNameArr });
         }
     });
