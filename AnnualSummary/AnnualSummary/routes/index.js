2de1062557c8bf4636e870a46c09b8f54289c1bf
刘悦 <liuyue@wanfangdata.com.cn>
2017-02-16 10:54:03 +0800

    解决乱码问题

--- AnnualSummary/AnnualSummary/routes/index.js
+++ AnnualSummary/AnnualSummary/routes/index.js
@@ -4,15 +4,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    fs.readFile('trellodata.json', function (err, data) {
+    fs.readFile('trellodata.json','utf8',function (err, data) {
         if (err) {
             return console.error(err);
         } else {
-            //  var data = JSON.parse(data.toString('utf8'));
             var string = JSON.parse(data);
             var fullNameArr = [];
             for (var i = 0; i < string.members.length; i++) {
-                var fullName = string.members[i].username;
+                var fullName = string.members[i].fullName;
                 fullNameArr.push(fullName);
             }
             res.render('index', { value: fullNameArr });
