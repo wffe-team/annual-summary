6b68d77a22e63736bf00a3822188a7e00ac10ad0
zhangkaidi <zhangkaidi@ZHANGKAIDI>
2017-02-16 15:44:11 +0800

    参与人数最多的项目（后台数据处理）

--- AnnualSummary/AnnualSummary/routes/index.js
+++ AnnualSummary/AnnualSummary/routes/index.js
@@ -9,12 +9,29 @@ router.get('/', function (req, res) {
             return console.error(err);
         } else {
             var obj = JSON.parse(data);
+
             //前端成员
             var fullNameArr = [];
             for (var i = 0; i < obj.members.length; i++) {
                 var fullName = obj.members[i].fullName;
                 fullNameArr.push(fullName);
             }
+
+            //参与项目人数最多
+            var mNumber = [];
+            for (var i = 0; i < obj.cards.length; i++) {
+                var membersLength = obj.cards[i].idMembers.length;
+                mNumber.push(membersLength);
+            }
+            //参与最多人数的个数
+            var maxNumber = Math.max.apply(null, mNumber);
+            //对应的索引
+            var maxNumberIndex = mNumber.indexOf(maxNumber); 
+            //参与人数最多的项目名称
+            var maxNumberItemName = obj.cards[maxNumberIndex].name;
+            //输出项目名称
+            console.log(maxNumberItemName);
+
             res.render('index', { value: fullNameArr });
         }
     });
