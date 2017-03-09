614ef52584365b4bc189dd7d1c767ab5c947b4f8
刘悦 <liuyue@wanfangdata.com.cn>
2017-03-09 11:02:45 +0800

    labels前台显示

--- AnnualSummary/AnnualSummary/public/src/js/initLabelsItems.js
+++ AnnualSummary/AnnualSummary/public/src/js/initLabelsItems.js
@@ -1,38 +1,45 @@
-﻿(function () {
+﻿(function () {   
     var labelsName = [];
-    var persoanlArr = [];
-    $('.careerArr').each(function () {
-        persoanlArrVal = $(this).val();
-        persoanlArr.push(persoanlArrVal);
-    });
-    var lablesNumber = [];
     $('.labelsName').each(function () {
         persoanlArrVal = $(this).val();
         labelsName.push(persoanlArrVal);
+    });
+    var itemnum;
+    var arrItems = [];
+    $('.labels-items').each(function () {
+        itemnum = $(this).val();
+        arrItems.push(itemnum)
     });
     var personalArr = [];
     for (var i = 0; i < arrItems.length - 1; i = i + 7) {
         personalArr.push([arrItems[i], arrItems[i + 1], arrItems[i + 2], arrItems[i + 3], arrItems[i + 4], arrItems[i + 5], arrItems[i + 6]]);
     }
-    console.log(personalArr)
+    var careerArr = [];
+    $('.careerArr').each(function () {
+        persoanlArrVal = $(this).val();
+        careerArr.push(persoanlArrVal);
+    });
+    careerArr.push('其他');
+
     var myChart = echarts.init(document.getElementById('labelsItemsL'));
     var option = {
         title: {
-            text: '基础雷达图'
+            
         },
         tooltip: {},
         legend: {
-            data: persoanlArr
+            data: labelsName
         },
         radar: {
             // shape: 'circle',
             indicator: [
-                { name: labelsName[0], max: 22 },
-                { name: labelsName[1], max: 16 },
-                { name: labelsName[2], max: 20 },
-                { name: labelsName[3], max: 20 },
-                { name: labelsName[4], max: 20 },
-                { name: labelsName[5], max: 20 }
+                { name: careerArr[0], max: 10 },
+                { name: careerArr[1], max: 16 },
+                { name: careerArr[2], max: 5 },
+                { name: careerArr[3], max: 5 },
+                { name: careerArr[4], max: 5 },
+                { name: careerArr[5], max: 5 },
+                { name: careerArr[6], max: 5 }
             ]
         },
         series: [{
@@ -42,27 +49,27 @@
             data: [
                 {
                     value: personalArr[0],
-                    name: persoanlArr[0]
+                    name: labelsName[0]
                 },
                 {
                     value: personalArr[1],
-                    name: persoanlArr[1]
+                    name: labelsName[1]
                 },
                 {
                     value: personalArr[2],
-                    name: persoanlArr[2]
+                    name: labelsName[2]
                 },
                 {
                     value: personalArr[3],
-                    name: persoanlArr[3]
+                    name: labelsName[3]
                 },
                 {
                     value: personalArr[4],
-                    name: persoanlArr[4]
+                    name: labelsName[4]
                 },
                 {
                     value: personalArr[5],
-                    name: persoanlArr[5]
+                    name: labelsName[5]
                 }
             ]
         }]
