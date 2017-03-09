fc7dbfb5aa915a2bce52ea3bca0d79dadaff7a80
刘悦 <liuyue@wanfangdata.com.cn>
2017-03-09 15:11:20 +0800

    update

--- AnnualSummary/AnnualSummary/public/src/js/initLabelsItems.js
+++ AnnualSummary/AnnualSummary/public/src/js/initLabelsItems.js
@@ -1,4 +1,5 @@
-﻿(function () {   
+﻿(function () { 
+//标签名  
     var labelsName = [];
     $('.labelsName').each(function () {
         persoanlArrVal = $(this).val();
@@ -10,16 +11,30 @@
         itemnum = $(this).val();
         arrItems.push(itemnum)
     });
+//数据
     var personalArr = [];
     for (var i = 0; i < arrItems.length - 1; i = i + 7) {
         personalArr.push([arrItems[i], arrItems[i + 1], arrItems[i + 2], arrItems[i + 3], arrItems[i + 4], arrItems[i + 5], arrItems[i + 6]]);
     }
+    //事业部
     var careerArr = [];
     $('.careerArr').each(function () {
         persoanlArrVal = $(this).val();
         careerArr.push(persoanlArrVal);
     });
     careerArr.push('其他');
+    //series
+    var seriesData = [];
+    for (var i = 0; i < personalArr[0].length; i++) {
+        var value = [];
+        for (var j = 0; j < personalArr.length; j++) {
+            value.push(personalArr[j][i]);
+        }
+        seriesData.push({
+            value: value,
+            name:careerArr[i]
+        })
+    }
 
     var myChart = echarts.init(document.getElementById('labelsItemsL'));
     var option = {
@@ -28,50 +43,23 @@
         },
         tooltip: {},
         legend: {
-            data: labelsName
+            data: careerArr
         },
         radar: {
             // shape: 'circle',
             indicator: [
-                { name: careerArr[0], max: 10 },
-                { name: careerArr[1], max: 16 },
-                { name: careerArr[2], max: 5 },
-                { name: careerArr[3], max: 5 },
-                { name: careerArr[4], max: 5 },
-                { name: careerArr[5], max: 5 },
-                { name: careerArr[6], max: 5 }
+                { name: labelsName[0], max: 10 },
+                { name: labelsName[3], max: 5 },
+                { name: labelsName[2], max: 5 },
+                { name: labelsName[1], max: 16 },
+                { name: labelsName[4], max: 5 }
             ]
         },
         series: [{
             name: '预算 vs 开销（Budget vs spending）',
             type: 'radar',
             // areaStyle: {normal: {}},
-            data: [
-                {
-                    value: personalArr[0],
-                    name: labelsName[0]
-                },
-                {
-                    value: personalArr[1],
-                    name: labelsName[3]
-                },
-                {
-                    value: personalArr[2],
-                    name: labelsName[2]
-                },
-                {
-                    value: personalArr[3],
-                    name: labelsName[1]
-                },
-                {
-                    value: personalArr[4],
-                    name: labelsName[4]
-                },
-                {
-                    value: personalArr[5],
-                    name: labelsName[5]
-                }
-            ]
+            data: seriesData
         }]
     };
     myChart.setOption(option);
